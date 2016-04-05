28eaf46b7b1c2479ede4939f43b0bdde4da0165b
Brian <brianflaherty64@gmail.com>
2016-04-05 14:34:43 -0400

    commented out deploy.sh scripts so that it does not actually deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
-DEPLOY_HOSTNAME=galaxy.meteor.com \
-  meteor deploy \
-    finblocks.meteorapp.com \
-    --settings settings-prod.json
+#! /bin/bash
+
+# DEPLOY_HOSTNAME=galaxy.meteor.com \
+#   meteor deploy \
+#     finblocks.meteorapp.com \
+#     --settings settings-prod.json
